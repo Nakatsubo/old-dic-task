f271744d66552ac3b17fde0e2a6f2db4da9637e6
NakatsuboYusuke <>
2019-07-18 15:18:23 +0900

    up new file

---
+++ html_css02/dist/assets/js/script.js
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+"use strict";var date=new Date,element=document.getElementById("time");function getTime(e){return data.getHours()+":"+e.getMinutes()+":"+e.getSeconds()}element.innerHTML=getTime(date);
+=======
+"use strict";
+
+$(function () {
+  $('.side_menu nav ul li a').each(function () {
+    var $href = $(this).attr('href');
+
+    if (location.href.match($href)) {
+      $(this).parent().addClass('current');
+    } else {
+      $(this).parent().removeClass('current');
+    }
+  });
+});
+>>>>>>> develop
+//# sourceMappingURL=maps/script.js.map
