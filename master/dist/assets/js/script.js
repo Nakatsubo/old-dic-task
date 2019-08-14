2f31641da7ce12ac1ef79230d31cf6e8f1fe74b0
NakatsuboYusuke <>
2019-08-14 18:26:33 +0900

    renewal folders & files

--- master/dist/assets/js/script.js
+++
@@ -1,2 +0,0 @@
-"use strict";var date=new Date,element=document.getElementById("time");function getTime(e){return data.getHours()+":"+e.getMinutes()+":"+e.getSeconds()}element.innerHTML=getTime(date);
-//# sourceMappingURL=maps/script.js.map
